a5740082fa98110fc14c658ea8260c504910e9fe
Rohit Kumar <rohit2kumar02@gmail.com>
2026-01-08 14:45:55 +0530

    main message

--- src/main/java/com/hotel/HotelLivyaApplication.java
+++ src/main/java/com/hotel/HotelLivyaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelLivyaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("HotelLivyaApplication.main()");
 		SpringApplication.run(HotelLivyaApplication.class, args);
 	}
 
